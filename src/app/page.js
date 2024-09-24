4b3ee91d1fbac926504489cea4b0ca4093236a81
AbuRaihan32 <abumahfuz3211@gmail.com>
2024-09-24 23:57:30 +0600

    Exam section added

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Banner from "@/components/Home/Banner/Banner";
 import DigitalAttendance from "@/components/Home/DigitalAttendance/DigitalAttendance";
+import Exam from "@/components/Home/Exam/Exam";
 import Overview from "@/components/Home/Overview/Overview";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <Banner></Banner>
       <Overview></Overview>
       <DigitalAttendance></DigitalAttendance>
+      <Exam></Exam>
     </>
   );
 }
