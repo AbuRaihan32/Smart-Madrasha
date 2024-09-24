e2caba50c448cdf546975d066bd6cf6b11c9bddd
AbuRaihan32 <abumahfuz3211@gmail.com>
2024-09-25 00:03:49 +0600

    Exam section updated

--- src/components/Home/Exam/Exam.jsx
+++ src/components/Home/Exam/Exam.jsx
@@ -4,7 +4,7 @@ import { RiCheckboxCircleFill } from "react-icons/ri";
 
 const Exam = () => {
   return (
-    <section className="bg-white my-10">
+    <section className="bg-slate-50 py-5 mb-10">
       <div className="max-w-6xl mx-auto py-8">
         <div className=" w-[94%] mx-auto flex items-center flex-row-reverse">
           <div className="w-full">
@@ -26,31 +26,19 @@ const Exam = () => {
             <ul className="mt-4 space-y-2">
               <li className="flex items-center gap-2 bg-white shadow rounded-lg p-[9px] text-[14px]">
                 <RiCheckboxCircleFill className="text-[#0098A8]"></RiCheckboxCircleFill>{" "}
-                <span> Attendance Display System</span>
+                <span> Flexible Setting</span>
               </li>
               <li className="flex items-center gap-2 bg-white shadow rounded-lg p-[9px] text-[14px]">
                 <RiCheckboxCircleFill className="text-[#0098A8]"></RiCheckboxCircleFill>{" "}
-                <span> Student, Teacher & Staff Report</span>
+                <span> Easy to Modify</span>
               </li>
               <li className="flex items-center gap-2 bg-white shadow rounded-lg p-[9px] text-[14px]">
                 <RiCheckboxCircleFill className="text-[#0098A8]"></RiCheckboxCircleFill>{" "}
-                <span> Entry SMS Confirmation</span>
+                <span> Result By SMS</span>
               </li>
               <li className="flex items-center gap-2 bg-white shadow rounded-lg p-[9px] text-[14px]">
                 <RiCheckboxCircleFill className="text-[#0098A8]"></RiCheckboxCircleFill>{" "}
-                <span> Exit SMS Confirmation</span>
-              </li>
-              <li className="flex items-center gap-2 bg-white shadow rounded-lg p-[9px] text-[14px]">
-                <RiCheckboxCircleFill className="text-[#0098A8]"></RiCheckboxCircleFill>{" "}
-                <span> Late SMS</span>
-              </li>
-              <li className="flex items-center gap-2 bg-white shadow rounded-lg p-[9px] text-[14px]">
-                <RiCheckboxCircleFill className="text-[#0098A8]"></RiCheckboxCircleFill>{" "}
-                <span> Absent And Bunk SMS</span>
-              </li>
-              <li className="flex items-center gap-2 bg-white shadow rounded-lg p-[9px] text-[14px]">
-                <RiCheckboxCircleFill className="text-[#0098A8]"></RiCheckboxCircleFill>{" "}
-                <span> Manage Schedule</span>
+                <span> Smart Result Card</span>
               </li>
             </ul>
           </div>
