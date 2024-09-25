f960911c01f4efd15fbac831535c7be2b996149e
AbuRaihan32 <abumahfuz3211@gmail.com>
2024-09-26 00:06:20 +0600

    Location section setup complete

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Account from "@/components/Home/Account/Account";
 import Banner from "@/components/Home/Banner/Banner";
 import DigitalAttendance from "@/components/Home/DigitalAttendance/DigitalAttendance";
 import Exam from "@/components/Home/Exam/Exam";
+import Location from "@/components/Home/Location/Location";
 import Overview from "@/components/Home/Overview/Overview";
 import StudentReport from "@/components/Home/StudentReport/StudentReport";
 import Testimonials from "@/components/Home/Testimonials/Testimonials";
@@ -16,6 +17,7 @@ export default function Home() {
       <Account></Account>
       <StudentReport></StudentReport>
       <Testimonials></Testimonials>
+      <Location></Location>
     </div>
   );
 }
---
+++ src/components/Home/Location/Location.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Location = () => {
+    return (
+        <div className='bg-slate-50 h-72 flex items-center justify-center w-full'>
+            <p className='text-4xl'>Here I will Add Google Map</p>
+        </div>
+    );
+};
+
+export default Location;
