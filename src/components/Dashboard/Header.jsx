1bf98080b20028cadb1f6ca094db1a82321f076f
AbuRaihan32 <abumahfuz3211@gmail.com>
2024-10-07 23:59:12 +0600

    dashboard header updated

--- src/components/Dashboard/Header.jsx
+++ src/components/Dashboard/Header.jsx
@@ -1,24 +1,27 @@
-import React from "react";
+import { IoMdCall } from "react-icons/io";
+import { MdLocationPin, MdMarkEmailRead } from "react-icons/md";
+import { HiOutlineMail } from "react-icons/hi";
+
 
 const Header = () => {
   return (
     <header className=" w-5/6 bg-[#00A12A] p-5 flex justify-between fixed right-0 text-white overflow-y-auto">
       <div>
         <h2 className="text-2xl font-bold">
-          আলহাজ্ব আবুল কাশেম ও মরহুমা ফাতেমা খাতুন <br /> মারকাযুল কুরআন মাদরাসা
+          আলহাজ্ব আবুল কাশেম ও মরহুমা ফাতেমা খাতুন মারকাযুল কুরআন মাদরাসা
           (2024-25)
         </h2>
-        <p className="">
-          Here&rsquo;s what&rsquo;s happening with your store today.
-        </p>
-      </div>
-      <div className="relative">
-        <input
-          type="text"
-          placeholder="Search for anything..."
-          className="pl-10 pr-4 py-2 rounded-full bg-gray-800 text-gray-300"
-        />
-        <span className="absolute left-3 top-2 text-gray-400">🔍</span>
+        <div className="flex items-center gap-2">
+          <p className="flex gap-1 items-center">
+            <MdLocationPin></MdLocationPin> <span> আদিতমারী, লালমনিরহাট </span>
+          </p>
+          <p className="flex gap-1 items-center">
+            <IoMdCall></IoMdCall> <span> +8801719107678 </span>
+          </p>
+          <p className="flex gap-1 items-center">
+            <HiOutlineMail></HiOutlineMail> <span> ar.mahfuj.dev@gmail.com </span>
+          </p>
+        </div>
       </div>
     </header>
   );
