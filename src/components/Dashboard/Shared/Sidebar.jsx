f23e2437e7dc7d1e845b7e57886cd96d20361f20
AbuRaihan32 <abumahfuz3211@gmail.com>
2024-10-12 02:49:38 +0600

    drowpdown added in dashboard sidebar

--- src/components/Dashboard/Shared/Sidebar.jsx
+++ src/components/Dashboard/Shared/Sidebar.jsx
@@ -10,12 +10,27 @@ import SidebarProfile from "../DashboardHome/SidebarProfile";
 import useToggle from "@/components/Hooks/useToggle";
 
 const Sidebar = () => {
-  const {toggle} = useToggle();
+  const { toggle } = useToggle();
   return (
-    <div className={`w-1/6 bg-[#273A28] p-5 flex flex-col h-screen justify-between fixed text-white overflow-y-auto shadow-2xl ${toggle ? 'hidden' : 'block'}`}>
+    <div
+      className={`w-1/6 bg-[#273A28] py-5 flex flex-col h-screen justify-between fixed text-white overflow-y-auto shadow-2xl ${
+        toggle ? "hidden" : "block"
+      }`}
+    >
       <SidebarProfile></SidebarProfile>
       <div>
         <nav className="space-y-4">
+          <details className="dropdown w-full border-t border-b border-[#1d2c1e] hover:bg-[#182c19]">
+            <summary className="m-1 ">Basic Settings</summary>
+            <ul className="menu dropdown-content rounded-box z-[1] p-2 shadow">
+              <li>
+                <a>Item 1</a>
+              </li>
+              <li>
+                <a>Item 2</a>
+              </li>
+            </ul>
+          </details>
           <Link
             href="#"
             className="flex items-center space-x-3 p-2 hover:bg-[#247983] rounded-lg"
