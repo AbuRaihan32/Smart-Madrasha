fab0d51a6a09b9bf7ea3a458b33611d75fcd2857
AbuRaihan32 <abumahfuz3211@gmail.com>
2024-10-24 12:29:31 +0600

    employee id card page added

---
+++ src/app/dashboard/teachers-and-staff/(settings)/employee-id-card/page.jsx
@@ -0,0 +1,9 @@
+
+import EmployeeIdCard from "@/components/Dashboard/PagesAboutTeacher&Staff/Setting/EmployeeIdCard";
+import React from "react";
+
+const EmployeeIdCardPage = () => {
+    return <EmployeeIdCard></EmployeeIdCard>
+};
+
+export default EmployeeIdCardPage;
---
+++ src/components/Dashboard/PagesAboutTeacher&Staff/Setting/EmployeeIdCard.jsx
@@ -0,0 +1,10 @@
+
+const EmployeeIdCard = () => {
+    return (
+        <div className="bg-white mt-6 md:mx-5 md:my-5 p-5 rounded-lg min-h-10 shadow-sm"> 
+            Hello this is Employee Id Card page
+        </div>
+    )
+};
+
+export default EmployeeIdCard;
